994ac0d38b04b3b871d5b6c899cbab8825772e4d
fanisridhar <99811717+fanisridhar@users.noreply.github.com>
2024-02-08 16:10:39 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors') 
 const app = express();
 app.use(cors()); 
-const port = 3000; 
+const port = 8080; 
 app.get('/', (req, res) => {
   res.send('Arithmetic service - Hello World!');
 }); 
